45aefc69fae79ac2d72f254ee985dd6062bc2678
Preethi Sriram <srirampreethi@outlook.com>
2022-01-17 09:51:35 -0800

    Add project files.

---
+++ Jump Game/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+static bool jumpgame(int[] nums)
+{
+    int n = nums.Length;
+    int reachable = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (i > reachable)
+        {
+            return false;
+        }
+       reachable = Math.Max(reachable, i+nums[i]);
+    }
+    return true;
+}
+
+int[] jumps = new int[] { 1, 1, 2, 5, 2, 1, 0, 0, 1, 3 };
+bool res = jumpgame(jumps);
+Console.WriteLine(res);
